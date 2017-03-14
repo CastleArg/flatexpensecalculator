70c02ed483240ed6da0932ef8d3bf0378f407d80
Mark Churcher <Mark Churcher>
2017-03-14 17:51:51 +1300

    initial commit

---
+++ FlatExpenseCalculator/Models/Property.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatExpenseCalculator.Models
+{
+    public class Property
+    {
+        public int Id;
+        public string Name;
+        public IAsyncEnumerable<Room> Rooms;
+        public IAsyncEnumerable<ApplicationUser> Tenants; 
+    }
+}
---
+++ FlatExpenseCalculator/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatExpenseCalculator.Models
+{
+    public class Room
+    {
+        public int Id;
+        public string Name;
+        public float AreaM2;
+        public IAsyncEnumerable<ApplicationUser> Occupants;
+    }
+}
